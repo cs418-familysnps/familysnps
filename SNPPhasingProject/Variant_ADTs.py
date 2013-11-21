033d1dc1a7559f7396e69013c2c2c3df2a91723b
kkintaro <katkintaro@gmail.com>
2013-11-21 16:07:44 -0700

    changing SNP to Variant

---
+++ SNPPhasingProject/Variant_ADTs.py
@@ -0,0 +1,18 @@
+'''An Enum for types of SNPs'''
+def enum(**enums):
+    return type('Enum', (), enums)
+
+VariantType = enum(HETEROZYGOUS = 0, HOMOZYGOUS = 1, SINGLESTRANDED = 2)
+
+'''Variant class, contains only the information needed to phase it'''
+class Variant:
+    myType = SNPType.HETEROZYGOUS
+    chromosome = -1
+    location = -1
+    
+    # could be bases or vcf indel notation
+    allele1 = "A"
+    allele2 = "B"
+	
+	def getAlleles():
+		return [allele1, allele2];
