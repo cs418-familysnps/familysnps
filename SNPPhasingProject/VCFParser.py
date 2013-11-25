8811070167d20201ec029f1791cf5853ec562f63
Jimmy <jimmyhales@outlook.com>
2013-11-25 14:32:07 -0700

    Now parsing our vcf file.

--- SNPPhasingProject/VCFParser.py
+++ SNPPhasingProject/VCFParser.py
@@ -2,6 +2,19 @@ import sys
 import os
 import vcf
 
-title = 'my title'
+# Look in the __init__ for some instructions on how to use this stuff.
 
-print title
+# Below I'm parsing through the first 5000 lines of our data.
+
+vcf_reader = vcf.Reader(open('top_5000_lines.vcf', 'r'))
+
+for record in vcf_reader:
+    print(record)
+    #Record.CHROM``
+    #Record.POS``
+    #Record.ID``
+    #Record.REF``
+    #Record.ALT``
+    #Record.QUAL``
+    #Record.FILTER``
+    #Record.INFO``    
