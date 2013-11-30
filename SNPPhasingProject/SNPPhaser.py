3e8672ecad4caaf5361bdcdf5f05575f00e425df
zygoth <zygoth2@gmail.com>
2013-11-30 14:22:37 -0700

    Finished code for SNPParser

--- SNPPhasingProject/SNPPhaser.py
+++ SNPPhasingProject/SNPPhaser.py
@@ -6,6 +6,45 @@ def phaseSNPs(motherVariantMap, fatherVariantMap, childVariantMap, outputFileNam
     
 
     for location in childVariantMap.keys():
-        print("do magic here")
+
         if childVariantMap[location].myType == VariantType.HOMOZYGOUS:
             continue
+
+        if variant.myType == VariantType.SINGLESTRANDED:
+
+            if fatherVariantMap[variant.location].myType == SINGLESTRANDED:
+                variant.allele1Source = "F"
+            else if motherVariantMap[variant.location].myType == SINGLESTRANDED:
+                variant.allele1Source = "M"
+            else:
+                variant.allele1Source = "U"
+
+            continue
+
+        #  This is the real meat of the algorithm
+        if variant.myType == VariantType.HETEROZYGOUS:
+
+            if fatherVariantMap[variant.location].myType == HOMOZYGOUS:
+
+                if fatherVariantMap[variant.location].allele1 == variant.allele1:
+
+                    variant.allele1Source = "F"
+                    variant.allele2Source = "M"
+                else:
+                    variant.allele1Source = "M"
+                    variant.allele2Source = "F"
+
+        else if motherVariantMap[variant.location].myType == HOMOZYGOUS:
+
+            if motherVariantMap[variant.location].allele1 == variant.allele1:
+
+                variant.allele1Source = "M"
+                variant.allele2Source = "F"
+            else:
+                variant.allele1Source = "F"
+                variant.allele2Source = "M"
+
+        else:     # In the case of parents both giving a different variant to their child, this would be phaseable, but that is rare and time is short.
+                  # Feel free to improve the code to take that case into account.
+            variant.allele1Source = "U"
+            variant.allele2Source = "U"
