79cbae4124c50711da2233270b3ba37dacbc2f2a
Dan <dbunked@gmail.com>
2013-12-02 09:39:23 -0700

    returns dictionary

--- SNPPhasingProject/23andme.py
+++ SNPPhasingProject/23andme.py
@@ -1,7 +1,10 @@
 import sys
+from Variant_ADTs import Variant
+from Variant_ADTs import VariantType
 
 SNPfile = open(sys.argv[1])
 
+dictionary = {}
 
 for line in SNPfile.readlines():
 	if line[0] != '#':
@@ -10,5 +13,29 @@ for line in SNPfile.readlines():
 		chromosome = fields[1]
 		position = fields[2]
 		alt = list(fields[3])
-		print "Record(CHROM=%s, POS=%s, REF=%s, ALT=%s)" % (chromosome, position, "?", alt)
+		#print "Record(CHROM=%s, POS=%s, REF=%s, ALT=%s)" % (chromosome, position, "?", alt)
+		variant = Variant()
 
+		if len(alt) == 1:
+			variant.myType = VariantType.SINGLESTRANDED
+		else:
+			if len(alt) == 2:
+				if alt[0] == alt[1]:
+					variant.myType = VariantType.HOMOZYGOUS
+				else:
+					variant.myType = VariantType.HETEROZYGOUS	
+
+
+		variant.location = ("chr%s" % (chromosome),position)
+		variant.allele1 = alt[0]
+		
+		if len(alt) == 2:
+			variant.allele2 = alt[1]
+		else:
+			variant.allele2 = alt[0]
+
+		dictionary[position] = variant
+
+
+print dictionary
+#return dictionary		
