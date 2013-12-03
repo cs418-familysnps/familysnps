dcb1d525031956b3af02da19ae4b551cccc0338c
Ben <zygoth2@gmail.com>
2013-12-02 21:45:05 -0700

    main stuff'

--- SNPPhasingProject/Main.py
+++ SNPPhasingProject/Main.py
@@ -1,4 +1,27 @@
 import sys
+from Referencer import referenceVariantMaps
+from VariantPhaser import phaseVariants
+
+# Given a valid filename of a vcf or 23andme file, returns a mapping of location to Variant
+def mapInputFile(fileName):
+    try:
+        inputFile = open(fileName)
+    except:
+        print(fileName, ' could not be opened. Aborting...')
+        exit(1)
+
+    splitFile = fileName.split('.')
+    fileType = splitFile[len(splitFile - 1)]
+    
+    if fileType != 'vcf' and fileType != 'txt':
+        print('File format is unsupported.  Only VCF or 23andMe formats are supported at this time.')
+        exit(1)
+
+    if fileType == 'vcf':
+        print("")#TODO: do VCF parsing here, and return the map
+
+    if fileType == 'txt':
+        print("")#TODO do 23andMe parsing here, and return the map
 
 motherFileName = ''
 fatherFileName = ''
@@ -7,9 +30,17 @@ pathToReference = ''
 
 if len(sys.argv) < 5:
     print('Arguments must be: motherFileName, fatherFileName, childFileName, pathToReference')
-    exit()
+    exit(1)
 
 motherFileName = sys.argv[1]
 fatherFileName = sys.argv[2]
 childFileName = sys.argv[3]
 pathToReference = sys.argv[4]
+
+motherVariantMap = mapInputFile(motherFileName)
+fatherVariantMap = mapInputFile(fatherFileName)
+childVariantMap = mapInputFile(childFileName)
+
+
+referenceVariantMaps(motherVariantMap, fatherVariantMap, childVariantMap)
+phaseVariants(motherVariantMap, fatherVariantMap, childVariantMap, 'phasedvariants.txt')
