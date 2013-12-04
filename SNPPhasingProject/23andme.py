7b165c7cad2558706737de1c0150cae94b5b3ec7
zygoth <zygoth2@gmail.com>
2013-12-03 17:00:30 -0700

    made into a method

--- SNPPhasingProject/23andme.py
+++ SNPPhasingProject/23andme.py
@@ -2,40 +2,41 @@ import sys
 from Variant_ADTs import Variant
 from Variant_ADTs import VariantType
 
-SNPfile = open(sys.argv[1])
+def parse23andmeFile(fileName):
+	
+	SNPfile = open(sys.argv[1])
 
-dictionary = {}
+	dictionary = {}
 
-for line in SNPfile.readlines():
-	if line[0] != '#':
-		line = line.rstrip()
-		fields = line.split('\t')
-		chromosome = fields[1]
-		position = fields[2]
-		alt = list(fields[3])
-		#print "Record(CHROM=%s, POS=%s, REF=%s, ALT=%s)" % (chromosome, position, "?", alt)
-		variant = Variant()
+	for line in SNPfile.readlines():
+		if line[0] != '#':
+			line = line.rstrip()
+			fields = line.split('\t')
+			chromosome = fields[1]
+			position = fields[2]
+			alt = list(fields[3])
+			#print "Record(CHROM=%s, POS=%s, REF=%s, ALT=%s)" % (chromosome, position, "?", alt)
+			variant = Variant()
 
-		if len(alt) == 1:
-			variant.myType = VariantType.SINGLESTRANDED
-		else:
+			if len(alt) == 1:
+				variant.myType = VariantType.SINGLESTRANDED
+			else:
+				if len(alt) == 2:
+					if alt[0] == alt[1]:
+						variant.myType = VariantType.HOMOZYGOUS
+					else:
+						variant.myType = VariantType.HETEROZYGOUS	
+	
+	
+			variant.location = ("chr%s" % (chromosome),position)
+			variant.allele1 = alt[0]
+			
 			if len(alt) == 2:
-				if alt[0] == alt[1]:
-					variant.myType = VariantType.HOMOZYGOUS
-				else:
-					variant.myType = VariantType.HETEROZYGOUS	
-
-
-		variant.location = ("chr%s" % (chromosome),position)
-		variant.allele1 = alt[0]
-		
-		if len(alt) == 2:
-			variant.allele2 = alt[1]
-		else:
-			variant.allele2 = alt[0]
-
-		dictionary[position] = variant
-
-
-print dictionary
-#return dictionary		
+				variant.allele2 = alt[1]
+			else:
+				variant.allele2 = alt[0]
+	
+			dictionary[position] = variant
+	
+	
+	return dictionary			
